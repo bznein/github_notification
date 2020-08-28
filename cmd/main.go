a58a204de4c547cbc4f76c284bf0b773099fb4a1
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-08-28 20:42:25 +0100

    First notification handling

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
-	"github.com/andybrewer/mack"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/andybrewer/mack"
 )
 
+type ApiResponse struct {
+	Id string `json:"id"`
+}
+
 func main() {
+	closeChan := make(chan bool)
+	go getNotifications(closeChan)
+	<-closeChan
+}
+
+func getNotifications(closeChan chan bool) {
 
 	token := os.Getenv("NOTIFICATION_TOKEN")
 	client := &http.Client{}
@@ -22,20 +34,26 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	req, err = http.NewRequest("GET", "https://api.github.com/notifications?all=true", nil)
+	req, err = http.NewRequest("GET", "https://api.github.com/notifications", nil)
 	req.Header.Add("Authorization", "token "+token)
-	response, err := client.Do(req)
 
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
+	for {
+		mack.Notify("Excuting")
+		response, err := client.Do(req)
+
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(responseData))
+		// do stuff
+		mack.Notify(string(responseData))
+		time.Sleep(time.Second * 60)
 	}
 
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(responseData))
-	// do stuff
-	mack.Notify("Complete")
 }
